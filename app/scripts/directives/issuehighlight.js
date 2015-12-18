7c53bbb602b84b47151ad0eb8b79e0872b5e2b85
Daniel Strebel <daniel.strebel@gmail.com>
2015-12-18 17:52:56 +0100

    highlight list

--- app/scripts/directives/issuehighlight.js
+++ app/scripts/directives/issuehighlight.js
@@ -6,22 +6,41 @@ angular.module('heinzelmannchen')
       templateUrl: 'views/issuehighlight.html',
       restrict: 'E',
       link: function postLink(scope, element, attrs) {
-        scope.highlightModel = {};
+        scope.highlightModel = {
+          terms: []
+        };
 
-        scope.searchByTerm = function() {
-          d3.selectAll("circle.pulse").classed('pulse', false);
-
-          var searchKey = scope.highlightModel.search;
-          var matches = d3.selectAll("circle[number='" + searchKey + "']");
+        function highlightTermMatch(searchKey) {
+          var matches = d3.selectAll('circle[number="' + searchKey + '"]');
           if(matches.length>0) {
             matches.classed('searched', true).classed('pulse', true);
           } else {
-            console.debug("No node found for search: " + searchKey);
+            console.debug('No node found for search: ' + searchKey);
           }
+        }
+
+        function clearHighlights() {
+          d3.selectAll('circle').classed('pulse', false).classed('searched', false);
+        }
+
+        scope.searchByTerm = function() {
+          d3.selectAll('circle.pulse').classed('pulse', false);
+          highlightTermMatch(scope.highlightModel.search);
+          scope.highlightModel.terms.push(scope.highlightModel.search);
+          scope.highlightModel.search = '';
         };
 
+        scope.removeTermAtIndex = function(i) {
+          scope.highlightModel.terms.splice(i, 1);
+          clearHighlights();
+          _.each(scope.highlightModel.terms, function(term) {
+            highlightTermMatch(term);
+          });
+        }
+
         scope.resetHighlights = function() {
-          d3.selectAll("circle").classed('pulse', false).classed('searched', false);
+          scope.highlightModel.terms = [];
+          clearHighlights();
         }
       }
     };
--- app/scripts/directives/onenter.js
+++ app/scripts/directives/onenter.js
@@ -4,12 +4,10 @@ angular.module('heinzelmannchen')
 .directive('onEnter', function () {
   return function (scope, element, attrs) {
     element.bind("keydown keypress", function (event) {
-      console.log('enter');
       if(event.which === 13) {
         scope.$apply(function (){
           scope.$eval(attrs.onEnter);
         });
-
         event.preventDefault();
       }
     });
