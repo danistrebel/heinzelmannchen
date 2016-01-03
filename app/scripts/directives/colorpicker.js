359aa0f8ea359122bea37a631bd0a87e7bce1ee1
Daniel Strebel <daniel.strebel@gmail.com>
2016-01-03 16:54:40 +0100

    Color picker/toggle for non compatible browsers

--- app/scripts/directives/colorpicker.js
+++ app/scripts/directives/colorpicker.js
@@ -14,6 +14,28 @@ angular.module('heinzelmannchen')
       scope: {
         color: '=',
         onColorChange: '='
+      },
+      link: function postLink(scope, element, attrs) {
+
+
+        var allColors = ['#64dd17', '#dc3912', '#ff9900', '#109618', '#990099', '#0099c6', '#dd4477'];
+
+        var input = document.createElement('input');
+        input.setAttribute('type', 'color');
+        scope.supportsColorInput = input.type==='color';
+
+        scope.toggleColor = function() {
+          if(scope.supportsColorInput) {
+            return; //let the color picker do the rest.
+          }
+
+          var currentColorIndex = allColors.indexOf(scope.color);
+
+          var nextColorIndex = ((currentColorIndex >= 0 ? currentColorIndex : 0) + 1) % allColors.length;
+          scope.color = allColors[nextColorIndex];
+          onColorChange();
+        }
+
       }
     };
   });
